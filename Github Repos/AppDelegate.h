d68a511f1bc5013d981ca952aec2cb70d7dec1d6
Marc Maguire <maguire.marc@gmail.com>
2017-05-22 14:09:15 -0400

    Initial Commit

---
+++ Github Repos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github Repos
+//
+//  Created by Marc Maguire on 2017-05-22.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
